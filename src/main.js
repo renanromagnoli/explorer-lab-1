588faff15dd7e67b9c2c229f6d4dcca6c28ec609
Renan Romagnoli <renanromagnoli@yahoo.com.br>
2022-10-22 08:22:09 -0300

    function setCard created

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import "./css/index.css"
+
+function setCard(flag) {
+  const bgColor1 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+  const bgColor2 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+  const logo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+  const colors = {
+    visa: ["blue", "lightblue"],
+    mastercard: ["orange", "lightorange"],
+    default: ["gray", "lightgray"],
+  }
+
+  bgColor1.setAttribute("fill", colors[flag][0])
+  bgColor2.setAttribute("fill", colors[flag][1])
+  logo.setAttribute("src", `${flag}.svg`)
+}
+
+globalThis.setCard = setCard
