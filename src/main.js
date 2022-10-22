ea5125c71bfb489a0a4f299cee7a8d2810ca2fa3
Renan Romagnoli <renanromagnoli@yahoo.com.br>
2022-10-22 10:46:44 -0300

    card number masked with IMask done

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./css/index.css"
+import IMask from "imask"
 
 function setCard(flag) {
   const bgColor1 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
@@ -15,5 +16,22 @@ function setCard(flag) {
   bgColor2.setAttribute("fill", colors[flag][1])
   logo.setAttribute("src", `${flag}.svg`)
 }
+
+const cardNumber = document.querySelector(".cc-number")
+const inputCardNumber = document.querySelector("input#card-number")
+const maskNumber = {
+  mask: "0000 0000 0000 0000",
+}
+
+function setCardNumber(number) {
+  cardNumber.textContent = number
+}
+function maskNumberCard(inputNumberCardValue) {
+  const mask = IMask(inputNumberCardValue, maskNumber)
+  setCardNumber(mask.value)
+}
+inputCardNumber.addEventListener("input", () => {
+  maskNumberCard(inputCardNumber)
+})
 
 globalThis.setCard = setCard
