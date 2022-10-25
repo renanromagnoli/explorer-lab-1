42dc72f4e914ade08088d7d38b955334df71d512
Renan Romagnoli <renanromagnoli@yahoo.com.br>
2022-10-25 08:49:17 -0300

    masks and regex added

--- src/main.js
+++ src/main.js
@@ -17,21 +17,50 @@ function setCard(flag) {
   logo.setAttribute("src", `${flag}.svg`)
 }
 
-const cardNumber = document.querySelector(".cc-number")
-const inputCardNumber = document.querySelector("input#card-number")
-const maskNumber = {
-  mask: "0000 0000 0000 0000",
+const cardNumber = document.querySelector("input#card-number")
+const maskCardNumber = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12/,
+      cardtype: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "default",
+    },
+  ],
+  dispatch: function (appended, dynamicMasked) {
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "")
+    const foundedMask = dynamicMasked.compiledMasks.find(function (item) {
+      return number.match(item.regex)
+    })
+    return foundedMask
+  },
 }
 
-function setCardNumber(number) {
-  cardNumber.textContent = number
+const cardNumberMask = IMask(cardNumber, maskCardNumber)
+
+const expirationDate = document.querySelector("#expiration-date")
+const expirationDateMask = {
+  mask: "MM{/}YY",
+  blocks: {
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
+    },
+  },
 }
-function maskNumberCard(inputNumberCardValue) {
-  const mask = IMask(inputNumberCardValue, maskNumber)
-  setCardNumber(mask.value)
-}
-inputCardNumber.addEventListener("input", () => {
-  maskNumberCard(inputCardNumber)
-})
 
 globalThis.setCard = setCard
